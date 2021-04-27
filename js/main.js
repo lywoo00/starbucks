52195989d72a9d5de868436a4dc0bfc957a0eec1
lywoo00 <lywoo00@naver.com>
2021-04-27 23:26:27 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 // SCROLL LODASH 
 const badgeEl = document.querySelector('.badges');
 const toTopEl = document.querySelector('#to-top');
@@ -140,8 +124,6 @@ spyEls.forEach(function (spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
 
 
 
